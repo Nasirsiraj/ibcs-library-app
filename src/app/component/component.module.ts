f9b7839db48d656e5d5148fb9fad8d77a4dd2fd2
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-01 19:41:14 +0600

    dashboard, error component created

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MaterialModule} from '../material/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    ErrorPageComponent
+  ],
   imports: [
     CommonModule,
     MaterialModule
