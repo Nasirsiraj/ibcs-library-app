01d42dd2b405341c4c6f42a6898496700a950fec
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-01 19:47:44 +0600

    footer component created

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MaterialModule} from '../material/material.module';
 import {RoutingComponent} from '../app-routing.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-   RoutingComponent
+   RoutingComponent,
+   FooterComponent
   ],
   imports: [
     CommonModule,
