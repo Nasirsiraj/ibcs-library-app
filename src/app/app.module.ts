ec30ab3d097f732f4b1e8d4189efe4d1d6cf0278
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-01 19:37:21 +0600

    root-store module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import {ComponentModule} from './component/component.module';
+import { RootStoreModule } from './root-store/root-store.module';
 
 @NgModule({
   declarations: [
@@ -19,11 +15,8 @@ import {ComponentModule} from './component/component.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([]),
-    StoreRouterConnectingModule.forRoot(),
-    ComponentModule
+    ComponentModule,
+    RootStoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
