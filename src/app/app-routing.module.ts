2b064b55c2d034b6da4ffe64b4b718fd235928a3
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-01 19:44:54 +0600

    routing module configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+import {ErrorPageComponent} from './component/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'error', component: ErrorPageComponent},
+  {path: '**', redirectTo: 'error'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponent = [
+  DashboardComponent,
+  ErrorPageComponent
+];
--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MaterialModule} from '../material/material.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-
+import {RoutingComponent} from '../app-routing.module';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent,
-    ErrorPageComponent
+   RoutingComponent
   ],
   imports: [
     CommonModule,
