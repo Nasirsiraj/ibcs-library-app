d6e33fa5bf0a52fd6548a8e5d38cab5f78ed0f28
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-03 21:45:26 +0600

    member model created

---
+++ src/app/model/member.model.ts
@@ -0,0 +1,13 @@
+export interface Member{
+  id: number;
+  name: string;
+  nid: number;
+  birthDate: string;
+  age: number;
+  email: string;
+  password: string;
+  address: string;
+  book: string;
+  issueDate: string;
+  fine: number;
+}
