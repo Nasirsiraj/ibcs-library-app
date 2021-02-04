35008aa8505f0b68d0d792ae0b83fdabf1536276
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-04 14:23:39 +0600

    jquery, bootstrap added

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -6,13 +6,16 @@ import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [
-   RoutingComponent,
-   FooterComponent
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule
-  ]
+    declarations: [
+        RoutingComponent,
+        FooterComponent
+    ],
+    exports: [
+        FooterComponent
+    ],
+    imports: [
+        CommonModule,
+        MaterialModule
+    ]
 })
 export class ComponentModule { }
