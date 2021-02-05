d4daf6e848ec8163a37bc227336fa2248ad865c2
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-05 17:31:52 +0600

    sidenav updated

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -8,21 +8,21 @@ import {Component, OnInit} from '@angular/core';
 export class DashboardComponent implements OnInit {
   constructor() { }
   sidenavOpened = true;
-  navLinkList = [
-    'Create Account',
-    'Browse Books',
-    'Borrow Books',
-    'Submit Books',
-    'Pay Fine',
-    'Contact',
-    'Help',
-    'Share',
-    'Settings'
-  ];
+  navLinkList = new Map([
+    [1, {title: 'Create Account', link: '/dashboard/create-account'}],
+    [2, {title: 'Browse Books', link: '/dashboard'}],
+    [3, {title: 'Borrow Books', link: '/dashboard'}],
+    [4, {title: 'Submit Books', link: '/dashboard'}],
+    [5, {title: 'Pay Fine', link: '/dashboard'}],
+    [6, {title: 'Contact', link: '/dashboard/contact'}],
+    [7, {title: 'Help', link: '/dashboard/help'}],
+    [8, {title: 'Share', link: '/dashboard/share'}],
+    [9, {title: 'Settings', link: '/dashboard/settings'}]
+  ])
 
   ngOnInit(): void {
   }
   toogleSidenav(): void{
-    this.sidenavOpened = !this.sidenavOpened;
+    this.sidenavOpened = !this.sidenavOpened
   }
 }
