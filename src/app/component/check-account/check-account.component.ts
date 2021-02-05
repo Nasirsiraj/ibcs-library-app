721cee9ea0c84d0d0fbfd5915ef09e118531ed71
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-05 19:12:28 +0600

    check-account c created

---
+++ src/app/component/check-account/check-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-account',
+  templateUrl: './check-account.component.html',
+  styleUrls: ['./check-account.component.scss']
+})
+export class CheckAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -4,12 +4,14 @@ import {MaterialModule} from '../material/material.module';
 import {AppRoutingModule, RoutingComponent} from '../app-routing.module';
 import { FooterComponent } from './footer/footer.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CheckAccountComponent } from './check-account/check-account.component';
 
 
 @NgModule({
     declarations: [
         RoutingComponent,
         FooterComponent,
+        CheckAccountComponent,
     ],
     exports: [
         FooterComponent
