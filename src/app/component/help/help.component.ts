6d158be2874cedca38bdfbe7cbbd2902050f9913
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-05 13:48:52 +0600

    help c coded

--- src/app/component/help/help.component.ts
+++ src/app/component/help/help.component.ts
@@ -8,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class HelpComponent implements OnInit {
 
   constructor() { }
+  lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, \n' +
+    'pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus. \n' +
+    'Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, \n' +
+    'in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent\n' +
+    'per conubia nostra, per inceptos himenaeos. Duis pharetra luctus lacus ut \n' +
+    'vestibulum. Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor.\n' +
+    'Integer eu nibh at nisi ullamcorper sagittis id vel leo. Integer feugiat \n' +
+    'faucibus libero, at maximus nisl suscipit posuere. Morbi nec enim nunc. \n' +
+    'Phasellus bibendum turpis ut ipsum egestas, sed sollicitudin elit convallis. \n' +
+    'Cras pharetra mi tristique sapien vestibulum lobortis. Nam eget bibendum metus, \n' +
+    'non dictum mauris. Nulla at tellus sagittis, viverra est a, bibendum metus.\n'
+
+  helpOption = new Map([
+    [1, 'How to create a library account'],
+    [2, 'How to see all available books'],
+    [3, 'How to borrow a book'],
+    [4, 'How to pay your fine'],
+    [5, 'All library rules']
+  ])
 
   ngOnInit(): void {
   }
-
 }
