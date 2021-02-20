95747357998421da9948806baa37a33513aa0579
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-20 15:15:35 +0600

    dashboard class updated

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -12,13 +12,12 @@ export class DashboardComponent implements OnInit {
     [1, {title: 'Create Account', link: '/dashboard/create-account'}],
     [2, {title: 'Check Account', link: '/dashboard/check-account'}],
     [3, {title: 'Browse Books', link: '/dashboard/browse-books'}],
-    [4, {title: 'Borrow Book', link: '/dashboard/borrow-book'}],
-    [5, {title: 'Return Book', link: '/dashboard/return-book'}],
-    [6, {title: 'Payment', link: '/dashboard/payment'}],
-    [7, {title: 'Contact', link: '/dashboard/contact'}],
-    [8, {title: 'Help', link: '/dashboard/help'}],
-    [9, {title: 'Share', link: '/dashboard/share'}],
-    [10, {title: 'Settings', link: '/dashboard/settings'}]
+    [4, {title: 'Return Book', link: '/dashboard/return-book'}],
+    [5, {title: 'Payment', link: '/dashboard/payment'}],
+    [6, {title: 'Contact', link: '/dashboard/contact'}],
+    [7, {title: 'Help', link: '/dashboard/help'}],
+    [8, {title: 'Share', link: '/dashboard/share'}],
+    [9, {title: 'Settings', link: '/dashboard/settings'}]
   ])
 
   ngOnInit(): void {
