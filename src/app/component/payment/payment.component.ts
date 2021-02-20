da602cbc9d41b620ac341b37694d2eb9663ec2f2
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-20 15:26:12 +0600

    payment c coded

--- src/app/component/payment/payment.component.ts
+++ src/app/component/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+  paymentForm = this.formBuilder.group({
+    nid: [null, [Validators.required]],
+    password: [null, [Validators.required]]
+  })
 
   ngOnInit(): void {
   }
+  onSubmit(value: any){
+    console.log(value)
+  }
 
+  // getters
+  get nid(){
+    return this.paymentForm.get('nid')
+  }
+  get password(){
+    return this.paymentForm.get('password')
+  }
 }
