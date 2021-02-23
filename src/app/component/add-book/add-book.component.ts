2e77aad38ef0af1f5c63d9694c69f3b9ad5e6c90
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-23 15:04:42 +0600

    add book coded

--- src/app/component/add-book/add-book.component.ts
+++ src/app/component/add-book/add-book.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {BookService} from '../../service/book.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Book} from '../../model/book.model';
 
 @Component({
   selector: 'app-add-book',
@@ -7,9 +11,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private bookService: BookService,
+    private snackBar: MatSnackBar
+  ) { }
+  isSubmitted = false
+  feedbackMessage = ''
+  isSucceed = false
+  isFailed = false
 
+  book: Book | null = null
+  addBookForm = this.formBuilder.group({
+    id: [null],
+    name: ['', [Validators.required]],
+    subject: ['', [Validators.required]],
+    writer: ['', [Validators.required]],
+    page: [null, [Validators.required]]
+  })
   ngOnInit(): void {
   }
+  onSubmitted(value: Book): void{
+    this.isSubmitted = true
+    try{
+      this
+        .bookService
+        .postOneBook(value)
+        .subscribe(
+          (response) => {
+            this.book = response
+            if(this.book == null){
+              this.isSucceed = false
+              this.isFailed = true
+              this.feedbackMessage = 'Error occurred!'
+              this.showSnackBar(this.feedbackMessage, 'Close')
 
+            } else if(this.book != null){
+              this.isSucceed = true
+              this.isFailed = false
+              this.feedbackMessage = 'Book Added!'
+              this.showSnackBar(this.feedbackMessage, 'Done')
+
+            }
+          },
+          (error) => {
+            this.isSucceed = false
+            this.isFailed = true
+            this.feedbackMessage = 'Server Error!'
+            this.showSnackBar(this.feedbackMessage, 'Close')
+          }
+        )
+    }catch (e){
+      this.isSucceed = false
+      this.isFailed = true
+      this.feedbackMessage = 'Error occurred!'
+      this.showSnackBar(this.feedbackMessage, 'Close')
+    }
+    this.addBookForm.reset()
+  }
+  refreshPage(): void{
+    window.location.reload()
+  }
+  showSnackBar(message: string, action: string): void{
+    this.snackBar.open(message, action, {duration: 5000})
+  }
+  // getters
+  get id(){
+    return this.addBookForm.get('id')
+  }
+  get name(){
+    return this.addBookForm.get('name')
+  }
+  get subject(){
+    return this.addBookForm.get('subject')
+  }
+  get writer(){
+    return this.addBookForm.get('writer')
+  }
+  get page(){
+    return this.addBookForm.get('page')
+  }
 }
--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Book} from '../model/book.model';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+  private apiUrl = environment.apiUrl
+
+  // get book by id
+  getBookById(id: number): Observable<Book | null>{
+    return this.httpClient.get<Book | null>(`${this.apiUrl}/book/${id}`)
+  }
+  // post one book
+  postOneBook(book: Book): Observable<Book | null>{
+    return this.httpClient.post<Book | null>(`${this.apiUrl}/book`, book)
+  }
 }
